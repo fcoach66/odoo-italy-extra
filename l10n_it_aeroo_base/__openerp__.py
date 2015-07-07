e353bb192fde00dd46456abd592b55e9afff7334
fcoach66 <fabrizio.cocciari@hotmail.it>
2015-07-07 08:21:09 +0200

    Add

---
+++ l10n_it_aeroo_base/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Base Module For Italy Like Aeroo Reports',
+    'version': '1.0',
+    'category': 'Localization/Italy',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Base Module For Italy Like Aeroo Reports
+==============================================
+    """,
+    'author':  'fcoach66',
+    'website': 'www.cocciari.com',
+    'images': [
+    ],
+    'depends': [
+        'report_aeroo',
+        'report_extended',
+    ],
+    'data': [
+        'report_configuration_defaults_data.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ l10n_it_aeroo_sale/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Italian Like Sale Order Aeroo Report',
+    'version': '1.0',
+    'category': 'Localization/Italy',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Italian Like Sale Order / Quotation Aeroo Report
+====================================================
+Parameters requirements:
+* total_discount: require module "sale_pricelist_discount"
+* print_validity: require module "sale_order_validity"
+    """,
+    'author':  'fcoach66',
+    'website': 'www.cocciari.com',
+    'images': [
+    ],
+    'depends': [
+        'report_extended_sale',
+        'l10n_it_aeroo_base',
+    ],
+    'data': [
+        'report_configuration_defaults_data.xml',
+        'sale_order_report.xml',
+        'sale_order_template.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
