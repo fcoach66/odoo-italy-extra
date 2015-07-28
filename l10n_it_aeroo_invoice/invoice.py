8bc2472a5fe6f01440732cd6b88f101cbad1a6ee
fcoach66 <fabrizio.cocciari@hotmail.it>
2015-07-28 14:05:48 +0200

    Mod

--- l10n_it_aeroo_invoice/invoice.py
+++ l10n_it_aeroo_invoice/invoice.py
@@ -35,3 +35,37 @@ def print_invoice_aeroo(self, cr, uid, ids, context=None):
     assert len(ids) == 1, 'This option should only be used for a single id at a time'
     self.signal_workflow(cr, uid, ids, 'quotation_sent')
     return self.pool['report'].get_action(cr, uid, ids, 'account.report_invoice_aeroo', context=context)
+
+def action_invoice_send_aeroo(self, cr, uid, ids, context=None):
+        '''
+        This function opens a window to compose an email, with the edi invoice template message loaded by default
+        '''
+        assert len(ids) == 1, 'This option should only be used for a single id at a time.'
+        ir_model_data = self.pool.get('ir.model.data')
+        try:
+            template_id = ir_model_data.get_object_reference(cr, uid, 'account', 'email_template_edi_invoice_aeroo')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference(cr, uid, 'mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = dict()
+        ctx.update({
+            'default_model': 'account.invoice',
+            'default_res_id': ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True
+        })
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
