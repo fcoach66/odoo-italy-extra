4173f692f8f99c568e6ade26477108d0149fc007
fcoach66 <fabrizio.cocciari@hotmail.it>
2016-06-30 08:25:20 +0200

    Add

---
+++ sale_additional_text_template/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+#
+#
+{"name": "Sale Additional Text Templates",
+ "summary": "Additional text on sale documents",
+ "version": "8.0.1.0.0",
+ "depends": ["base"],
+ "author": "fcoach66",
+ "data": ["additional_text_view.xml",
+          'security/ir.model.access.csv',
+          ],
+ "category": "Sale",
+ "installable": True,
+ "active": False, }
---
+++ sale_additional_text_template/additional_text.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+#
+#
+from openerp import models, fields, api
+
+
+class SaleAdditionalTextTemplate(models.Model):
+    _name = "sale.additional_text.template"
+    _description = "Sale Additional Text template"
+
+    name = fields.Char('Additional text name', required=True)
+    type = fields.Selection([('subjext', 'Subject'),
+                                 ('description', 'Description'),('limitation', 'Limitation')],
+                                'Type',
+                                required=True,
+                                default='subject',
+                                help="Type of additional text")
+    text = fields.text('Text', translate=True, required=True)
+
+    @api.multi
+    def get_value(self, partner_id=False):
+        self.ensure_one()
+        lang = None
+        if partner_id:
+            lang = self.env['res.partner'].browse(partner_id).lang
+        return self.with_context({'lang': lang}).text
