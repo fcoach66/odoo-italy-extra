c27dc3e29d24a1d04e8a72c9d728681c3e9a2ee4
fcoach66 <fabrizio.cocciari@hotmail.it>
2016-07-01 07:33:15 +0200

    Mod

--- l10n_it_aeroo_sale/__openerp__.py
+++ l10n_it_aeroo_sale/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Italian Sale Order Aeroo Report',
-    'version': '1.4',
+    'version': '1.5',
     'category': 'Localization/Italy',
     'sequence': 14,
     'summary': '',
