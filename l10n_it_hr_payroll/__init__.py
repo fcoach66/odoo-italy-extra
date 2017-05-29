5e6eb4ca0988a1456d907339b819b548a16b822c
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-05-29 14:55:28 +0200

    Add

---
+++ l10n_it_hr_payroll/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+#  File: __init__.py
+#  Module: l10n_it_hr_payroll
+#
+##############################################################################
+
+import hr_contract
+import hr_employee
