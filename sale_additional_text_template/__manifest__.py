064bf5f6cf3d49bf37c2798d69c5cfcb4914d5af
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-06 22:28:52 +0200

    Mod

--- sale_additional_text_template/__manifest__.py
+++ sale_additional_text_template/__manifest__.py
@@ -4,7 +4,7 @@
 #
 {"name": "Sale Additional Text Templates",
  "summary": "Additional text on sale documents",
- "version": "8.0.1.0.0",
+ "version": "10.0.1.0.0",
  "depends": ["base","sale"],
  "author": "fcoach66",
  "data": ["additional_text_view.xml",
