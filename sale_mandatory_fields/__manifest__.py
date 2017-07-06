066068687c82040793a3f5771a96ef54635a2c7f
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-06 22:21:54 +0200

    Mod

--- sale_mandatory_fields/__manifest__.py
+++ sale_mandatory_fields/__manifest__.py
@@ -4,7 +4,7 @@
 #
 {"name": "Sale Mandatory Fields",
  "summary": "Sale Order change mandatory fields",
- "version": "8.0.1.0.0",
+ "version": "10.0.1.0.0",
  "depends": ["base","sale","website_quote"],
  "author": "fcoach66",
  "data": ["sale_mandatory_fields_view.xml",
