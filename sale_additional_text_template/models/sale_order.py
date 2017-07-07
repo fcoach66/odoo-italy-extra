ae1fdccf9bc2ab23ac2b5677edf52decd9e59188
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-08 01:14:18 +0200

    Mod

--- sale_additional_text_template/models/sale_order.py
+++ sale_additional_text_template/models/sale_order.py
@@ -14,7 +14,7 @@ class SaleOrder(models.Model):
 	additional_text_3 = fields.Text('Limitation')
 
 	@api.multi
-	def set_additional_text(self, cr, uid, cond_id, field_name, partner_id):
+	def set_additional_text(self, cond_id, field_name, partner_id):
 		if not cond_id:
 			return {'value': {field_name: ''}}
 		cond_obj = self.pool['sale.additional_text.template']
@@ -22,14 +22,14 @@ class SaleOrder(models.Model):
 		return {'value': {field_name: text}}
 	
 	@api.multi
-	def set_additional_text_1(self, cr, uid, so_id, cond_id, partner_id):
-		return self.set_additional_text(cr, uid, cond_id, 'additional_text_1', partner_id)
+	def set_additional_text_1(self, so_id, cond_id, partner_id):
+		return self.set_additional_text(cond_id, 'additional_text_1', partner_id)
 	
 	@api.multi
-	def set_additional_text_2(self, cr, uid, so_id, cond_id, partner_id):
-		return self.set_additional_text(cr, uid, cond_id, 'additional_text_2', partner_id)
+	def set_additional_text_2(self, so_id, cond_id, partner_id):
+		return self.set_additional_text(cond_id, 'additional_text_2', partner_id)
 	
 	@api.multi
-	def set_additional_text_3(self, cr, uid, so_id, cond_id, partner_id):
-		return self.set_additional_text(cr, uid, cond_id, 'additional_text_3', partner_id)
+	def set_additional_text_3(self, so_id, cond_id, partner_id):
+		return self.set_additional_text(cond_id, 'additional_text_3', partner_id)
 
