a4d3ede5dcd3e14da53187fe7a3b16eed3592a6a
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-07 15:08:36 +0200

    Mod

--- sale_additional_text_template/models/sale_order.py
+++ sale_additional_text_template/models/sale_order.py
@@ -6,22 +6,12 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
 
 	_inherit = "sale.order"
-	_columns = {
-		'additional_text_template1_id': fields.Many2one(
-			'sale.additional_text.template',
-			'Subject'),
-		'additional_text_template2_id': fields.Many2one(
-			'sale.additional_text.template',
-			'Description'),
-		'additional_text_template3_id': fields.Many2one(
-			'sale.additional_text.template',
-			'Limitation'),
-		
-		'additional_text_1': fields.Text('Subject'),
-		'additional_text_2': fields.Text('Description'),
-		'additional_text_3': fields.Text('Limitation'),
-		
-	}
+	additional_text_template1_id': fields.Many2one('sale.additional_text.template','Subject')
+	additional_text_template2_id': fields.Many2one('sale.additional_text.template','Description')
+	additional_text_template3_id': fields.Many2one('sale.additional_text.template','Limitation')
+	additional_text_1': fields.Text('Subject')
+	additional_text_2': fields.Text('Description')
+	additional_text_3': fields.Text('Limitation')
 
 	@api.multi
 	def set_additional_text(self, cr, uid, cond_id, field_name, partner_id):
@@ -30,13 +20,16 @@ class SaleOrder(models.Model):
 		cond_obj = self.pool['sale.additional_text.template']
 		text = cond_obj.get_value(cr, uid, cond_id, partner_id)
 		return {'value': {field_name: text}}
-
+	
+	@api.multi
 	def set_additional_text_1(self, cr, uid, so_id, cond_id, partner_id):
 		return self.set_additional_text(cr, uid, cond_id, 'additional_text_1', partner_id)
-
+	
+	@api.multi
 	def set_additional_text_2(self, cr, uid, so_id, cond_id, partner_id):
 		return self.set_additional_text(cr, uid, cond_id, 'additional_text_2', partner_id)
-
+	
+	@api.multi
 	def set_additional_text_3(self, cr, uid, so_id, cond_id, partner_id):
 		return self.set_additional_text(cr, uid, cond_id, 'additional_text_3', partner_id)
 
