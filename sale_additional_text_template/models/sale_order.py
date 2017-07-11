e86984b285e067e746281964ab17a5c25fda0026
fcoach66 <fabrizio.cocciari@hotmail.it>
2017-07-11 19:41:31 +0200

    Mod

--- sale_additional_text_template/models/sale_order.py
+++ sale_additional_text_template/models/sale_order.py
@@ -16,20 +16,19 @@ class SaleOrder(models.Model):
 
         @api.onchange('additional_text_template1_id')
         def _set_additional_text_1(self):
-		    additional_text = self.additional_text_template1_id
-			if additional_text:
-			    self.additional_text_1 = additional_text.get_value(self.partner_id.id)
+            additional_text = self.additional_text_template1_id
+            if additional_text:
+                self.additional_text_1 = additional_text.get_value(self.partner_id.id)
 
         @api.onchange('additional_text_template2_id')
         def _set_additional_text_2(self):
-		    additional_text = self.additional_text_template2_id
-			if additional_text:
-			    self.additional_text_2 = additional_text.get_value(self.partner_id.id)
+            additional_text = self.additional_text_template2_id
+            if additional_text:
+                self.additional_text_2 = additional_text.get_value(self.partner_id.id)
 
         @api.onchange('additional_text_template3_id')
         def _set_additional_text_3(self):
-		    additional_text = self.additiona3_text_template1_id
-			if additional_text:
-			    self.additional_text_3 = additional_text.get_value(self.partner_id.id)
+            additional_text = self.additiona3_text_template1_id
+            if additional_text:
+                self.additional_text_3 = additional_text.get_value(self.partner_id.id)
 				
-
