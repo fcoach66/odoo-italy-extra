3356955e98dd4ff18e66ab754f12fa5864596909
fcoach66 <fabrizio.cocciari@hotmail.it>
2018-06-12 10:48:53 +0200

    Add

---
+++ l10n_it_aeroo_expense/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Italian Expense Aeroo Report',
+    'version': '1.6',
+    'category': 'Localization/Italy',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Italian Expense Aeroo Report
+    """,
+    'author':  'fcoach66',
+    'website': 'www.cocciari.com',
+    'images': [
+    ],
+    'depends': [
+        'l10n_it_aeroo_base',
+        'report_custom_filename',
+	],
+    'data': [
+        'expense_report.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
