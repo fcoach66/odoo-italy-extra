c098ec719cbf884c25acbb44dc1930b86d84ba40
fcoach66 <fabrizio.cocciari@hotmail.it>
2018-06-13 12:35:03 +0200

    Mod

---
+++ l10n_it_aeroo_expense/expense_note.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, timedelta
+import time
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
+import openerp.addons.decimal_precision as dp
+from openerp import workflow
+
+
