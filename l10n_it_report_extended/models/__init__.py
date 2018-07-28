ed07c9439c9dc01516966a3c8845ad90b46fd280
fcoach66 <fabrizio.cocciari@hotmail.it>
2018-07-28 20:38:21 +0200

    Mod

--- l10n_it_report_extended/models/__init__.py
+++ l10n_it_report_extended/models/__init__.py
@@ -3,5 +3,8 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from . import report
+from . import ir_actions_report
 from . import company
+from . import report_configuration_default
+from . import report_configuration_line
+from . import report_parser
