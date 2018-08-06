bf7677cb5a8580b8d519fa6a18854c2e715ae250
fcoach66 <fabrizio.cocciari@hotmail.it>
2018-08-06 09:29:24 +0200

    Mod

--- l10n_it_aeroo_ddt/ddt.py
+++ l10n_it_aeroo_ddt/ddt.py
@@ -5,7 +5,7 @@ class StockDdT(models.Model):
         _inherit = 'stock.picking.package.preparation'
         _description = 'DdT'
 
-        def print_ddt(self, cr, uid, ids, context=None):
-                assert len(ids) == 1, 'This option should only be used for a single id at a time'
+        @api.multi
+        def print_ddt(self):
                 return self.env.ref('l10n_it_aeroo_ddt.action_aeroo_report_it_ddt').report_action(self)
 
