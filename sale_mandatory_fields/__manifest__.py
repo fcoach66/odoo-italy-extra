8ae7fdb264edf1e607db9066508dc9cecdebd376
fcoach66 <fabrizio.cocciari@hotmail.it>
2018-11-03 15:56:38 +0100

    Mod

--- sale_mandatory_fields/__manifest__.py
+++ sale_mandatory_fields/__manifest__.py
@@ -7,7 +7,7 @@
  "version": "10.0.1.0.0",
  "depends": ["base","sale","website_quote"],
  "author": "fcoach66",
- "data": ["sale_mandatory_fields_view.xml",
+ "data": ["views/sale_mandatory_fields_view.xml",
           ],
  "category": "Sale",
  "installable": True,
