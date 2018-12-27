b9cec272c52c9eb66c19698c93edf7d3e71757a1
fcoach66 <fabrizio.cocciari@hotmail.it>
2018-12-27 15:00:14 +0100

    Add

---
+++ hr_expense_mandatory_fields/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+#
+#
+{"name": "HR Expense Mandatory Fields",
+ "summary": "HR Expense change mandatory fields",
+ "version": "8.0.1.0.0",
+ "depends": ["hr_expense"],
+ "author": "fcoach66",
+ "data": ["hr_expense_mandatory_fields_view.xml",
+          ],
+ "category": "Human Resources",
+ "installable": True,
+ "active": False, }
