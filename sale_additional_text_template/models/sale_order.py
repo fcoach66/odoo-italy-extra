97b5ffefe2b5c5cfbf2fe0ff1bd7569767b480d8
fcoach66 <fabrizio.cocciari@hotmail.it>
2019-10-23 09:39:15 +0200

    Update sale_order.py

--- sale_additional_text_template/models/sale_order.py
+++ sale_additional_text_template/models/sale_order.py
@@ -1,30 +1,29 @@
 from odoo import fields, models, api
 class SaleOrder(models.Model):
 	_inherit = "sale.order"
-	additional_text_template1_id = fields.Many2one('sale.additional_text.template', string='Subject')
-	additional_text_template2_id = fields.Many2one('sale.additional_text.template', string='Description')
-	additional_text_template3_id = fields.Many2one('sale.additional_text.template', string='Limitation')
+	additional_text_template1_id = fields.Many2one('sale.additional_text.template', string='Subject Templae')
+	additional_text_template2_id = fields.Many2one('sale.additional_text.template', string='Description Template')
+	additional_text_template3_id = fields.Many2one('sale.additional_text.template', string='Limitation Template')
 	additional_text_1 = fields.Text('Subject')
 	additional_text_2 = fields.Text('Description')
 	additional_text_3 = fields.Text('Limitation')
 
-	@api.multi
-	def set_additional_text(self, cr, uid, cond_id, field_name, partner_id):
-		if not cond_id:
-			return {'value': {field_name: ''}}
-		cond_obj = self.pool['sale.additional_text.template']
-		Text = cond_obj.get_value(cr, uid, cond_id, partner_id)
-		return {'value': {field_name: Text}}
+	@api.onchange('additional_text_template1_id')
+    def _additional_text_1(self):
+        additional_text = self.additional_text_template1_id
+        if additional_text:
+            self.additional_text_1 = additional_text.get_value(self.partner_id.id)
 
-	@api.multi
-	def set_additional_text_1(self, cr, uid, so_id, cond_id, partner_id):
-		return self.set_additional_text(cr, uid, cond_id, 'additional_text_1', partner_id)
+	@api.onchange('additional_text_template2_id')
+    def _additional_text_2(self):
+        additional_text = self.additional_text_template2_id
+        if additional_text:
+            self.additional_text_2 = additional_text.get_value(self.partner_id.id)
 
-	@api.multi		
-	def set_additional_text_2(self, cr, uid, so_id, cond_id, partner_id):
-		return self.set_additional_text(cr, uid, cond_id, 'additional_text_2', partner_id)
+	@api.onchange('additional_text_template3_id')
+    def _additional_text_3(self):
+        additional_text = self.additional_text_template3_id
+        if additional_text:
+            self.additional_text_3 = additional_text.get_value(self.partner_id.id)
 
-	@api.multi		
-	def set_additional_text_3(self, cr, uid, so_id, cond_id, partner_id):
-		return self.set_additional_text(cr, uid, cond_id, 'additional_text_3', partner_id)
 
