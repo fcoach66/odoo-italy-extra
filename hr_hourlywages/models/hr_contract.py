b8d2bd332a88a4386b63a553e642bfaba8ba4ac8
fcoach66 <fabrizio.cocciari@hotmail.it>
2021-01-29 09:47:14 +0100

    Update hr_contract.py

--- hr_hourlywages/models/hr_contract.py
+++ hr_hourlywages/models/hr_contract.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields
-
+import odoo.addons.decimal_precision as dp
 
 class HrContract(models.Model):
     _inherit = 'hr.contract'
 
-    hourlywage = fields.Monetary('Hourly Wage', digits=(16, 2), required=True, track_visibility="onchange", help="Employee's Hourly wage.", default= 0)
+    hourlywage = fields.Float('Hourly Wage', digits=dp.get_precision('Hourly Wage'), required=True, track_visibility="onchange", help="Employee's Hourly wage.", default= 0)
