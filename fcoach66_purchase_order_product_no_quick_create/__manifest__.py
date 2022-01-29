a99896e15323e37eb4640b7ba727330bdf5002f9
fcoach66 <fabrizio.cocciari@hotmail.it>
2022-01-29 20:27:09 +0100

    New module

---
+++ fcoach66_purchase_order_product_no_quick_create/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+#
+#
+{"name": "Purchase Order Product No Quick Create",
+ "summary": "Purchase Order Product No Quick Create",
+ "version": "12.0.1.0.0",
+ "depends": ["base","purchase"],
+ "author": "fcoach66",
+ "data": ["views/purchase_order_view.xml",
+          ],
+ "category": "Purchase",
+ "installable": True,
+ "active": False, }
---
+++ fcoach66_sale_order_product_no_quick_create/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+#
+#
+{"name": "Sale Order Product No Quick Create",
+ "summary": "Sale Order Product No Quick Create",
+ "version": "12.0.1.0.0",
+ "depends": ["base","sale","sale_quotation_builder"],
+ "author": "fcoach66",
+ "data": ["views/sale_order_view.xml",
+          ],
+ "category": "Sale",
+ "installable": True,
+ "active": False, }
