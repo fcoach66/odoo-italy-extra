cc89c35e751feaccd70ccdc201456ff84704c9d0
fcoach66 <fabrizio.cocciari@hotmail.it>
2022-02-07 20:19:53 +0100

    Added module

---
+++ fcoach66_purchase_ux/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Purchase UX (fcoach66)',
+    'version': '12.0.1.0.0',
+    'category': 'Purchase',
+    'summary': '',
+    'author': 'fcoach66',
+    'license': 'AGPL-3',
+    'depends': [
+        'purchase_ux',
+    ],
+    'data': [
+        'views/purchase_order_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
